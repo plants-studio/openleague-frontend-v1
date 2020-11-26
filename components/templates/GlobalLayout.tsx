88c610eaa521d0b235a41883c2f73203f579a749
plantstoen <plantstoen@gmail.com>
2020-11-27 02:52:11 +0900

    Add : Sass support

--- components/templates/GlobalLayout.tsx
+++ components/templates/GlobalLayout.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { css } from '@emotion/react';
+//import useWindowSize from './../../utils/useWindowSize';
+import './GlobalLayout.module.scss';
 
 interface IProps {
   children: React.ReactNode;
@@ -7,44 +8,17 @@ interface IProps {
 
 const GlobalLayout = ({ children }: IProps) => {
   return (
-    <div>
-      <div css={viewWrapper}>
-        <div css={leftNavigator}>무언가 내용</div>
-        <div style={{ paddingLeft: '440px', flex: '1' }}>
-          <div css={contentArea}>{children}</div>
-        </div>
+    <div className="view-wrapper">
+      {500 >= 400 ? (
+        <div className="leftnavigator">무언가 내용</div>
+      ) : (
+        <div>ss</div>
+      )}
+      <div style={{ paddingLeft: '440px', flex: '1' }}>
+        <div className="contents-area">{children}</div>
       </div>
     </div>
   );
 };
-
-const viewWrapper = css`
-  flex: 1;
-  position: relative;
-  display: flex;
-`;
-
-const leftNavigator = css`
-  position: fixed;
-  height: 100vh;
-  width: 440px;
-  min-width: 440px;
-  background-color: white;
-  border-radius: 0px 20px 20px 0px;
-  box-shadow: 0px 6px 25px #d1d5df;
-`;
-
-const contentArea = css`
-  padding: 2rem 2rem;
-`;
-
-const paddingSelector = (props: boolean) =>
-  props
-    ? css`
-        padding: 1rem 1rem;
-      `
-    : css`
-        padding: 0;
-      `;
 
 export default GlobalLayout;
---
+++ utils/useWindowSize.ts
@@ -0,0 +1,15 @@
+import { useLayoutEffect, useState } from 'react';
+
+const useWindowSize = () => {
+  const [size, setSize] = useState<number | null>(null);
+
+  useLayoutEffect(() => {
+    const updateSize = () => {
+      setSize(window.innerWidth);
+    };
+    updateSize();
+    window.addEventListener('resize', updateSize);
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+};
