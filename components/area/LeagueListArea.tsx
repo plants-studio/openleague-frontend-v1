fa3a05c1df4499fce06da72605a5935b486b63d5
plantstoen <plantstoen@gmail.com>
2020-11-30 22:42:56 +0900

    QUICK PUSH

--- components/area/LeagueListArea.tsx
+++ components/area/LeagueListArea.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import LeaguePreviewCard from './../cards/LeaguePreviewCard';
+import style from './LeagueListArea.module.scss';
 
 // TODO map 방식으로 동작되는 리스트 만들기
 const LeagueListArea = () => {
   return (
-    <div>
+    <div className={style.container}>
+      <LeaguePreviewCard />
+      <LeaguePreviewCard />
       <LeaguePreviewCard />
     </div>
   );
--- components/cards/LeaguePreviewCard.tsx
+++ components/cards/LeaguePreviewCard.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Card, Header, SubHeader, Progress } from 'plants-ui';
+import { Card, Header, SubHeader, Progress, Sticker } from 'plants-ui';
 import style from './LeaugePreviewCard.module.scss';
 
 const LeaguePreviewCard = () => {
   return (
-    <Card width="30%" isPadding={false}>
+    <Card isPadding={false}>
       <div>
-        <div></div>
-        <div>
+        <div className={style.iamgeWrapper}>
+          <Sticker backgroundColor="#0CA76A" top="1rem" left="1rem">
+            LEAGUE OF LEGENDS
+          </Sticker>
+          <img
+            className={style.image}
+            src="/testimage.jpg"
+            alt="테스트이미지"
+          ></img>
+        </div>
+        <div className={style.contentArea}>
           <Header size="small">가나다라</Header>
+          <SubHeader size="small">마바사</SubHeader>
+          <Progress />
         </div>
       </div>
     </Card>
