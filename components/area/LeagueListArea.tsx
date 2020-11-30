c21e7d5d9a9dfb9aa106e8ea449761e0c1212a6c
plantstoen <plantstoen@gmail.com>
2020-12-01 01:33:37 +0900

    QUICK PUSH

--- components/area/LeagueListArea.tsx
+++ components/area/LeagueListArea.tsx
@@ -6,9 +6,42 @@ import style from './LeagueListArea.module.scss';
 const LeagueListArea = () => {
   return (
     <div className={style.container}>
-      <LeaguePreviewCard />
-      <LeaguePreviewCard />
-      <LeaguePreviewCard />
+      <LeaguePreviewCard
+        game="LEAGUE OF LEGEND"
+        title="이건 대회이름입니다"
+        placeType="오프라인"
+        teamReqMemCnt={5}
+        leagueSchedule="11월 20일"
+        applicationDeadline={3}
+        percentage={50}
+        applicant={35}
+        applicantMinMax="80명 이상 100명 이하"
+        color="#0CA76A"
+      />
+      <LeaguePreviewCard
+        game="LEAGUE OF LEGEND"
+        title="이건 대회이름입니다 그런데 너무많은 정보를 곁들인"
+        placeType="오프라인"
+        teamReqMemCnt={5}
+        leagueSchedule="11월 20일"
+        applicationDeadline={3}
+        percentage={50}
+        applicant={35}
+        applicantMinMax="80명 이상 100명 이하"
+        color="#FC9826"
+      />
+      <LeaguePreviewCard
+        game="LEAGUE OF LEGEND"
+        title="이건 대회이름입니다"
+        placeType="오프라인"
+        teamReqMemCnt={5}
+        leagueSchedule="11월 20일"
+        applicationDeadline={3}
+        percentage={50}
+        applicant={35}
+        applicantMinMax="80명 이상 100명 이하"
+        color="#5F5F5F"
+      />
     </div>
   );
 };
--- components/cards/LeaguePreviewCard.tsx
+++ components/cards/LeaguePreviewCard.tsx
@@ -2,13 +2,38 @@ import React from 'react';
 import { Card, Header, SubHeader, Progress, Sticker } from 'plants-ui';
 import style from './LeaugePreviewCard.module.scss';
 
-const LeaguePreviewCard = () => {
+interface IProps {
+  game: string;
+  title: string;
+  placeType: string;
+  teamReqMemCnt: number;
+  leagueSchedule: string;
+  applicationDeadline: number;
+  percentage: number;
+  applicant: number;
+  applicantMinMax: string;
+  color: string;
+}
+
+// TODO 바로 구현되어있는 스타일 UI라이브러리로 옮기기
+const LeaguePreviewCard = ({
+  game,
+  title,
+  placeType,
+  teamReqMemCnt,
+  leagueSchedule,
+  applicationDeadline,
+  percentage,
+  applicant,
+  applicantMinMax,
+  color,
+}: IProps) => {
   return (
     <Card isPadding={false}>
       <div>
         <div className={style.iamgeWrapper}>
-          <Sticker backgroundColor="#0CA76A" top="1rem" left="1rem">
-            LEAGUE OF LEGENDS
+          <Sticker backgroundColor={color} top="1rem" left="1rem">
+            {game}
           </Sticker>
           <img
             className={style.image}
@@ -17,9 +42,36 @@ const LeaguePreviewCard = () => {
           ></img>
         </div>
         <div className={style.contentArea}>
-          <Header size="small">가나다라</Header>
-          <SubHeader size="small">마바사</SubHeader>
-          <Progress />
+          <div className={style.headerArea}>
+            <Header size="small">{title}</Header>
+            <SubHeader size="small">
+              {placeType} · {teamReqMemCnt}인 · {leagueSchedule}
+            </SubHeader>
+          </div>
+
+          <div className={style.bottomInfoArea}>
+            <Progress color={color} width="100%" percentage={percentage} />
+            <div className={style.bottomInfoArea__applicationInfoArea}>
+              <div>
+                <span
+                  className={style.bottomInfoArea__applicant}
+                  style={{ color: color }}
+                >
+                  {applicant}
+                </span>
+                <span
+                  className={style.bottomInfoArea__applicantMinMax}
+                  style={{ color: color }}
+                >
+                  {' '}
+                  / {applicantMinMax}
+                </span>
+              </div>
+              <span className={style.bottomInfoArea__deadline}>
+                {applicationDeadline}일 남음
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </Card>
